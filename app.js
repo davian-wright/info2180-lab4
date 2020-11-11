fe686b843c933ac9ec665ad1d44795ff87f81077
davian-wright <71860216+davian-wright@users.noreply.github.com>
2020-11-10 22:11:29 -0500

    Edited starting files

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ function loadDoc() {
   xhttp.open("GET", "superheroes.php", true);
   xhttp.send();
 }
+
+
+function loadOne(str){
+  var xhttp;
+  if (str.length == 0) { 
+    document.getElementById("result").innerHTML = superheroes.php;
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("result").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "superheroes.php?query="+str, true);
+  xhttp.send();   
+}
